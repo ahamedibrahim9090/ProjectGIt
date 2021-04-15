ed0e36bd5291faf7cf7bf1cdf09e07d7bcfad22a
ahamedibrahim9090 <ahamedaqr765@gmail.com>
2021-04-15 05:55:35 +0530

    project git files added

---
+++ src/test/java/org/product/MenuCode.java
@@ -0,0 +1,12 @@
+package org.product;
+
+public class MenuCode {
+
+	public static void main(String[] args) {
+		System.out.println("method1");
+		System.out.println("method2");
+		System.out.println("method3");
+		
+	}
+
+}
