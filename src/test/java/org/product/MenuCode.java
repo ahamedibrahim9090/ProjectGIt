6b09eb028905357400b6324e095a3bdca0cd3e80
ahamedibrahim9090 <ahamedaqr765@gmail.com>
2021-04-15 06:14:33 +0530

    updated code by ahamed

--- src/test/java/org/product/MenuCode.java
+++ src/test/java/org/product/MenuCode.java
@@ -6,6 +6,9 @@ public class MenuCode {
 		System.out.println("method1");
 		System.out.println("method2");
 		System.out.println("method3");
+		System.out.println("method4");
+		System.out.println("method5");
+		System.out.println("method6");
 		
 	}
 
